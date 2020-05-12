0c23022ee75eeadbebb386b9b6c1e47ac407050a
neverzhai <zhaiyiding1990@sina.cn>
2020-05-13 00:02:47 +0800

    init project

---
+++ filter/src/test/java/com/demo/filter/FilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
