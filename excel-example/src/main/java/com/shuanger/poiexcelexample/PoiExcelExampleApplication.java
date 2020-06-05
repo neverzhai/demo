065e3284fb95c2bb6fc0937c43f30050b107c972
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-06-05 10:59:37 +0800

    add excel example module

---
+++ excel-example/src/main/java/com/shuanger/poiexcelexample/PoiExcelExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.poiexcelexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiExcelExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiExcelExampleApplication.class, args);
+    }
+
+}
---
+++ excel-example/src/test/java/com/shuanger/poiexcelexample/PoiExcelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.poiexcelexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiExcelExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
