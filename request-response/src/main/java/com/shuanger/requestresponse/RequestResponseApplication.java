9387af9cbc1c20812670f2ee4735044404d22940
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-06-24 15:03:02 +0800

    add new module

---
+++ request-response/src/main/java/com/shuanger/requestresponse/RequestResponseApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.requestresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestResponseApplication.class, args);
+    }
+
+}
---
+++ request-response/src/test/java/com/shuanger/requestresponse/RequestResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.requestresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
