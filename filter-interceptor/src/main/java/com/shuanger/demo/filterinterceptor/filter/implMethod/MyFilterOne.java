e6b51c230335ade75b01f92245621f6cd3c628f4
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-06-28 20:42:35 +0800

    add impl.

--- filter-interceptor/src/main/java/com/shuanger/demo/filterinterceptor/filter/implMethod/MyFilterOne.java
+++ filter-interceptor/src/main/java/com/shuanger/demo/filterinterceptor/filter/implMethod/MyFilterOne.java
@@ -1,13 +1,23 @@
 package com.shuanger.demo.filterinterceptor.filter.implMethod;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author: zhaixiaoshuang
  * @date: 2020-06-28 20:12
  * @description:
  */
-@WebFilter
+@WebFilter(urlPatterns = "/filter/*")
 public class MyFilterOne extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilter(request, response, chain);
+    }
 }
--- filter-interceptor/src/main/java/com/shuanger/demo/filterinterceptor/filter/implMethod/MyFilterTwo.java
+++ filter-interceptor/src/main/java/com/shuanger/demo/filterinterceptor/filter/implMethod/MyFilterTwo.java
@@ -2,7 +2,12 @@ package com.shuanger.demo.filterinterceptor.filter.implMethod;
 
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author: zhaixiaoshuang
@@ -11,4 +16,9 @@ import javax.servlet.http.HttpFilter;
  */
 @Component
 public class MyFilterTwo extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilter(request, response, chain);
+    }
 }
