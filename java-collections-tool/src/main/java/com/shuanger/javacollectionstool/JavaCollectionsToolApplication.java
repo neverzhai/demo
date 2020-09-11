2d3e83ba49dc4217bf82f584358950913f010026
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-09-11 16:12:20 +0800

    add java java collections module

---
+++ java-collections-tool/src/main/java/com/shuanger/javacollectionstool/JavaCollectionsToolApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.javacollectionstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionsToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCollectionsToolApplication.class, args);
+    }
+
+}
---
+++ java-collections-tool/src/test/java/com/shuanger/javacollectionstool/JavaCollectionsToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.javacollectionstool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCollectionsToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
