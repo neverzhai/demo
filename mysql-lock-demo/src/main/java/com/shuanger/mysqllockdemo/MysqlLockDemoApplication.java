74321de0339fbf45480d629d4f35f36e16553681
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-12-09 10:17:22 +0800

    add mysql lock demo module

---
+++ mysql-lock-demo/src/main/java/com/shuanger/mysqllockdemo/MysqlLockDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.mysqllockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlLockDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlLockDemoApplication.class, args);
+    }
+
+}
---
+++ mysql-lock-demo/src/test/java/com/shuanger/mysqllockdemo/MysqlLockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.mysqllockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlLockDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
