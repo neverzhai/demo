ddfa8391008e3a355bfea3768f5ae7c93bbc76d2
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-12-09 10:24:14 +0800

    add auto

---
+++ mysql-lock-demo/src/main/java/com/shuanger/mysqllockdemo/auto/MyAutoGenerator.java
@@ -0,0 +1,82 @@
+package com.shuanger.mysqllockdemo.auto;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.Scanner;
+
+public class MyAutoGenerator {
+
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+//        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir("D:\\joycity-points");
+        gc.setAuthor("zhaixiaoshuang");
+        gc.setOpen(false);
+        mpg.setGlobalConfig(gc);
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306:3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("dreamfinal21");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("joycity");
+        pc.setMapper("dao");
+        pc.setEntity("domain");
+        pc.setParent("com.jdcity");
+        mpg.setPackageInfo(pc);
+
+
+
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+//        strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+//        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+        //sys_user,sys_member,sys_user_member,sys_manager,sys_manager_permission,cfg_city,cfg_place,biz_traffic_info,biz_daily_health_info
+    }
+
+
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StrUtil.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+}
