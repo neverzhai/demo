42b181e50e59d1b5992bcec75bb668fed31eb72e
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-12-18 15:27:22 +0800

    add completatble future demo module

---
+++ completatbl-future-demo/src/main/java/com/shuanger/completatblfuturedemo/CompletatblFutureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.completatblfuturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletatblFutureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompletatblFutureDemoApplication.class, args);
+    }
+
+}
---
+++ completatbl-future-demo/src/test/java/com/shuanger/completatblfuturedemo/CompletatblFutureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.completatblfuturedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletatblFutureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
