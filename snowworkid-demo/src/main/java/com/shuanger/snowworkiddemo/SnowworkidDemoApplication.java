952e5ca2274ac8b2b68b2dfc1fb5de963164f54f
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-12-28 11:36:46 +0800

    add snow work id demo module

---
+++ snowworkid-demo/src/main/java/com/shuanger/snowworkiddemo/SnowworkidDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.snowworkiddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowworkidDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowworkidDemoApplication.class, args);
+    }
+
+}
---
+++ snowworkid-demo/src/test/java/com/shuanger/snowworkiddemo/SnowworkidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.snowworkiddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowworkidDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
