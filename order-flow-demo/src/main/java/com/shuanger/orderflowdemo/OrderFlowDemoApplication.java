c000d23edb296a230d19cbc5ffe0553527983728
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2020-12-28 11:54:03 +0800

    add order demo module

---
+++ order-flow-demo/src/main/java/com/shuanger/orderflowdemo/OrderFlowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shuanger.orderflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFlowDemoApplication.class, args);
+    }
+
+}
---
+++ order-flow-demo/src/test/java/com/shuanger/orderflowdemo/OrderFlowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuanger.orderflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
