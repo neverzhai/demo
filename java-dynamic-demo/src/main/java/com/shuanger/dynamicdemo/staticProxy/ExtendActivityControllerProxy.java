c9a3134275f9fcae4ba5e93b013b6c0d7691979d
neverzhai <zhaiyiding1990@sina.cn>
2021-01-21 09:03:17 +0800

    add dynamic demo

---
+++ java-dynamic-demo/src/main/java/com/shuanger/dynamicdemo/staticProxy/ExtendActivityControllerProxy.java
@@ -0,0 +1,9 @@
+package com.shuanger.dynamicdemo.staticProxy;
+
+/**
+ * @author: zhaixiaoshuang
+ * @date: 2021-01-20 22:32
+ * @description: 通过继承的方式实现代理
+ */
+public class ExtendActivityControllerProxy {
+}
---
+++ java-dynamic-demo/src/main/java/com/shuanger/dynamicdemo/staticProxy/InterfaceActivityControllerProxy.java
@@ -0,0 +1,10 @@
+package com.shuanger.dynamicdemo.staticProxy;
+
+/**
+ * @author: zhaixiaoshuang
+ * @date: 2021-01-20 22:29
+ * @description: 基于实现接口实现动态代理
+ *              适用于有接口的情况下， 如果我们使用的第三方库，而要代理的对象并没有定义接口则无法使用， 需要通过继承类来实现
+ */
+public class InterfaceActivityControllerProxy {
+}
